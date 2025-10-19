06ffe312329fce8224e1886440e6f72418834cef
BubbleFish <daforsastudia@gmail.com>
2025-10-19 14:10:45 +0300

    feat: Fix update_eyecons.sh script

--- update_eyecons.sh
+++ update_eyecons.sh
@@ -1,12 +1,12 @@
 EYECONS_DIR=/tmp/eyecons
 
-# LOCATION=$(curl -s https://api.github.com/repos/bbfh-dev/eyecons/releases/latest | jq -r '.assets.[0].browser_download_url')
-# echo $LOCATION
-# curl -L -o /tmp/eyecons.tar.gz $LOCATION
-# rm -rf $EYECONS_DIR
-# mkdir -p $EYECONS_DIR
-# tar -xzf /tmp/eyecons.tar.gz -C $EYECONS_DIR
-# ls $EYECONS_DIR/symbolic/web/ > $EYECONS_DIR/INDEX
+LOCATION=$(curl -s https://api.github.com/repos/bbfh-dev/eyecons/releases/latest | jq -r '.assets.[0].browser_download_url')
+echo $LOCATION
+curl -L -o /tmp/eyecons.tar.gz $LOCATION
+rm -rf $EYECONS_DIR
+mkdir -p $EYECONS_DIR
+tar -xzf /tmp/eyecons.tar.gz -C $EYECONS_DIR
+ls $EYECONS_DIR/symbolic/web/ > $EYECONS_DIR/INDEX
 
 echo "" > ./src/gen/__eyecons.html
 for file in ${EYECONS_DIR}/symbolic/web/*; do
@@ -17,3 +17,7 @@ for file in ${EYECONS_DIR}/symbolic/web/*; do
     cat ${EYECONS_DIR}/symbolic/web/${name}.svg >> ./src/gen/__eyecons.html
     echo '</button>' >> ./src/gen/__eyecons.html
 done
+
+git add .
+git commit -m "chore: Update eyecons"
+git push origin main
