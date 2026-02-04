3d290ce5d63f310360688afebeeff666aeea8532
BubbleFish <daforsastudia@gmail.com>
2026-02-04 14:16:46 +0300

    feat: Add artwork page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,12 @@
-const root = document.querySelector("#root");
+/** @type {HTMLDivElement} */
+const root_element = document.getElementById("root");
 
 /**
  * @param {boolean} is_right
  */
 function scrollOnMobile(is_right) {
-	root.scrollTo({
-		left: is_right ? root.scrollLeftMax : 0,
+	root_element.scrollTo({
+		left: is_right ? root_element.scrollLeftMax : 0,
 		top: 0,
 		behavior: "smooth",
 	});
